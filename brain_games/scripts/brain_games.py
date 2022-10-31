d2a3abebffb82ba375aeb1054ec941ff5d4a44b5
Sergei Ezerskii <gaftezer@gmail.com>
2022-10-31 11:28:27 +0300

    step 3 needs fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], '/home/user/hexlet-project1/brain_games/'))
+
+from cli import welcome_user
+
 
 def greet():
 	print('Welcome to the Brain Games!')
@@ -7,7 +13,7 @@ def greet():
 
 def main():
 	greet()
-
+	welcome_user()
 
 if __name__ == '__main__':
 	main()

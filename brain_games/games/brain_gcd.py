adf1e33eaf9a2f2e1950b3fd516ada8441396828
Sergei Ezerskii <gaftezer@gmail.com>
2022-11-09 10:11:49 +0300

    Refactoring brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,6 +1,4 @@
-from brain_games.main import get_username, get_question, welcome_user, end
 from random import randint
-import prompt
 
 
 def find_gcd(a, b):
@@ -13,22 +11,9 @@ def find_gcd(a, b):
     return gcd
 
 
-def gcd():
-    ROUNDS = 3
-    print('Welcome to the Brain Games!')
-    username = get_username()
-    welcome_user(username)
-    print('Find the greatest common divisor of given numbers.')
-    while ROUNDS:
-        a = randint(1, 100)
-        b = randint(1, 100)
-        question = f'{a} {b}'
-        get_question(question)
-        answer = prompt.string('Your answer: ')
-        result = find_gcd(a, b)
-        if answer == str(result):
-            print('Correct!')
-            ROUNDS -= 1
-        else:
-            break
-    end(ROUNDS, username, answer, result)
+def gcd_game():
+    a = randint(1, 100)
+    b = randint(1, 100)
+    question = f'{a} {b}'
+    result = find_gcd(a, b)
+    return question, result
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-from brain_games.games.brain_gcd import gcd
+
+from brain_games.main import game
+from brain_games.games.brain_gcd import gcd_game
+
+
+game_begin = 'Find the greatest common divisor of given numbers.'
 
 
 def main():
-    gcd()
+    game(game_begin, gcd_game)
 
 
 if __name__ == '__main__':

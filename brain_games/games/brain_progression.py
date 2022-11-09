af0b66d8a04bd0c2a41709c2a8b08e2eef01bee0
Sergei Ezerskii <gaftezer@gmail.com>
2022-11-09 10:33:42 +0300

    Refactoring brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,6 +1,4 @@
-from brain_games.main import get_question, get_username, welcome_user, end
 from random import randint
-import prompt
 
 
 def make_progression():
@@ -12,23 +10,10 @@ def make_progression():
     return prog
 
 
-def progression():
-    ROUNDS = 3
-    print('Welcome to the Brain Games!')
-    username = get_username()
-    welcome_user(username)
-    print('What number is missing in the progression?')
-    while ROUNDS:
-        prog = make_progression()
-        idx = randint(0, len(prog) - 1)
-        question = " ".join(str(el) for el in prog).replace(str(prog[idx]),
-                                                            '..', 1)
-        get_question(question)
-        answer = prompt.string('Your answer: ')
-        result = str(prog[idx])
-        if answer == str(result):
-            print('Correct!')
-            ROUNDS -= 1
-        else:
-            break
-    end(ROUNDS, username, answer, result)
+def progression_game():
+    prog = make_progression()
+    idx = randint(0, len(prog) - 1)
+    question = " ".join(str(el) for el in prog).replace(str(prog[idx]),
+                                                        '..', 1)
+    result = str(prog[idx])
+    return question, result
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-from brain_games.games.brain_progression import progression
+
+from brain_games.main import game
+from brain_games.games.brain_progression import progression_game
+
+
+game_begin = 'What number is missing in the progression?'
 
 
 def main():
-    progression()
+    game(game_begin, progression_game)
 
 
 if __name__ == '__main__':
